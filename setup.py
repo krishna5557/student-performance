b06e379a9f8cdd845d9974f8440b7bc345ddc006
K G KRISHNAKANTH <95517599+krishna5557@users.noreply.github.com>
2023-04-18 23:28:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_requirements(file_path:str)->List[str]:
         requirements=[req.replace("\n","") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
-            requirements.remove('-e .)
+            requirements.remove('-e .')
     
     return requirements
 
